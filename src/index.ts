a711ab15e9dbb9a420c701744d03b0ea5c61f749
Paulo Mariano <paulomariano2501@gmail.com>
2024-02-08 11:44:33 -0300

    🚀 RELEASE: Start project

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+// src/index.ts
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Bem-vindo ao meu servidor Node.js com TypeScript e CORS habilitado!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado na porta ${PORT}`);
+});
