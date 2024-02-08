437d2acfa472d2ccb6668ba64e287589f88896b6
Paulo Mariano <paulomariano2501@gmail.com>
2024-02-08 13:40:18 -0300

    📦 NEW: add app routes to server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 // src/index.ts
 import express from "express";
 import cors from "cors";
+import { appRoutes } from "./application/infra";
 
 const app = express();
 
@@ -13,6 +14,8 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
+
+app.use(appRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor iniciado na porta ${PORT}`);
