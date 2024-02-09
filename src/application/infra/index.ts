140e2c443a80c30bb311e21b04a5e204df6783c0
Paulo Mariano <paulomariano2501@gmail.com>
2024-02-09 13:26:04 -0300

    📦 NEW: add swagger docs

--- src/application/infra/index.ts
+++ src/application/infra/index.ts
@@ -2,9 +2,12 @@ import { Router } from "express";
 import { producerRoutes } from "./producer/producer-routes";
 import { farmRoutes } from "./farm/farm-routes";
 import { dashboardRoutes } from "./dashboard/dashboard-routes";
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./../../swagger.json";
 
 const appRoutes = Router();
 
+appRoutes.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 appRoutes.use("/producer", producerRoutes);
 appRoutes.use("/farm", farmRoutes);
 appRoutes.use("/dashboard", dashboardRoutes);
